abe8ed817cdbcb32ef617345fd5b5309c52de5ed
IvSanti <iscastanedah@uqvirtual.edu.co>
2020-05-30 00:57:05 -0500

    report memory RAM

--- scripts/generarReporte.py
+++ scripts/generarReporte.py
@@ -31,12 +31,12 @@ def getData(comand, n):
 
 
 
-def addMemory(info):
+def addMemoryHDD(info):
 	memory = []
 	memory.append("Información de Disco Duro\n")
 	memory.append("Se muestra Información del disco duro\n")
-	memory.append("Nombre del disco\n")
-	memory.append("Espacio\n")
+	memory.append("Magnitud\n")
+	memory.append("Valor en MB\n")
 	memory.append("Bar\n")
 	memory.append("Espacio Total\n")
 	memory.append(str(info[1])+"\n")
@@ -45,13 +45,15 @@ def addMemory(info):
 	memory.append("Espacio Libre\n")
 	memory.append(str(info[6]))
 	memory.append("fin\n")
-	print(memory)
 	return memory
 
 def addProcess(info):
 	process = []
-	process.append("Recursos del proceso con PID " + str(info[0]) + "\n")
-	process.append("Agujas\n")
+	process.append("Información de consumo de recursos")
+	process.append("Se muestra información del proceso con PID " + str(info[0]) + "\n")
+	process.append("Recurso\n")
+	process.append("Valor\n")
+	process.append("Pie\n")
 	process.append("Memoria Virtual\n")
 	process.append(str(info[4])+"\n")
 	process.append("Memoria RAM\n")
@@ -66,29 +68,54 @@ def addProcess(info):
 	return process
 
 
+def addMemoryRAM(info):
+	memory = []
+	memory.append("Información de Memoria RAM\n")
+	memory.append("Se muestra Información de la memoria RAM\n")
+	memory.append("Magnitud\n")
+	memory.append("Valor en KB\n")
+	memory.append("Area\n")
+	for i in info:
+		memory.append(str(i)+"\n")
+	memory.append("fin\n")
+	return memory
+
+
 def writeReport(data):
 	report = open("./report.txt","w", encoding="utf-8")
-
-	for line in data:
-		report.write(line)
+	i=0
+	while(i<len(data)):
+		for line in data[i]:
+			report.write(line)
+		i=i+1
 	report.close()
 
 #-------------------------------------------------------------------------#
 
 data = []
+
 infoMem = getData("free -m", 1)
 info = infoMem.split(" ")
+data.append(addMemoryHDD(info))
 
-data = addMemory(info)
-writeReport(data)
-"""
 n=3
 while (n>0):
 	infoPro = getData("top -n1", n)
 	info = infoPro.split(" ")
-
-	data = addProcess(info)
-	writeReport(data)
+	data.append(addProcess(info))
 	n = n-1
-"""
+
+n=15
+info2 = []
+while (n>0):
+	infoRAM = getData("cat /proc/meminfo", n)
+	info = infoRAM.split(" ")
+	info2.append(info[0])
+	info2.append(info[1])
+	n = n-1
+
+data.append(addMemoryRAM(info2))
+
+writeReport(data)
+
 print("Se ejecutó con éxito")
